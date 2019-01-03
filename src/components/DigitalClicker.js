4a4cd6103acdde26885e2bb914ace734b46a916c
Adam N <adam.nawroc@gmail.com>
2019-01-03 17:02:45 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,17 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+  info = () => {
+      console.log('Entering password...');
+    }
+
+  render() {
+    return (
+      <div>
+        <input onKeyUp={this.info} type="password" />
+      </div>
+    )
+  }
+}
